976bbb17e4c42f01fac434d29305fcaf8571967b
0mny5 <adel_chrono_48@yahoo.co.jp>
2024-07-12 20:14:11 +0900

    Add: 要ログインページにログアウトボタンを設置

--- src/app/[locale]/secret/page.tsx
+++ src/app/[locale]/secret/page.tsx
@@ -2,13 +2,21 @@
 
 import {useTranslations} from 'next-intl';
 import PageLayout from '../../../components/PageLayout';
+import {signOut} from 'next-auth/react';
 
 export default function Secret() {
   const t = useTranslations('Secret');
 
+  function onLogoutClick() {
+    signOut();
+  }
+
   return (
     <PageLayout title={t('title')}>
       <p>{t('description')}</p>
+      <button onClick={onLogoutClick} type="button">
+      {t('logout')}
+      </button>
     </PageLayout>
   );
 }
